9977e1cc788bba1b15579fbb97773c1edc79efb0
vignesh gr <141708172+vgr2004@users.noreply.github.com>
2024-03-04 10:50:49 +0530

    adding

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/ping', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Pong!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost: ${port} `);
+  console.log(`Server is running on ${port}`);
 });
