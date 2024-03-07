ebfed2f8595f4d9d5a29ac8a95f77fa70f9edb06
vignesh gr <141708172+vgr2004@users.noreply.github.com>
2024-03-07 15:25:15 +0530

    first component added

---
+++ Frontend/Spinechillz/src/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignIn from './Components/SignIn';
+
+const LandingPage = () => {
+  return (
+    <>
+    <div>
+      <h1>Spinechillz</h1>
+      <SignIn/>
+    </div>
+    </>
+  );
+};
+
+export default LandingPage;
